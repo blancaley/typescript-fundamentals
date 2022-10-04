0671461885c8c0e96d76e34906b54305d2b3ddb2
Blanca Ley <blanca.ley@outlook.com>
2022-10-04 15:14:46 +0200

    Return values and types

--- src/4-Creating-And-Using-Functions/output.ts
+++ src/4-Creating-And-Using-Functions/output.ts
@@ -1,5 +1,11 @@
 const prefix = '🐉 ';
 
+type ProductType = {
+  id: number;
+  name: string;
+  icon?: string;
+}
+
 export default async function updateOutput(id: string) {
   // Todo
 }
@@ -8,6 +14,7 @@ export default async function updateOutput(id: string) {
 runTheLearningSamples();
 
 function runTheLearningSamples() {
+  // Specifying typed parameters
   function displayProductInfo(id: number, name: string) {
     console.log(`${prefix} typed parameters`);
     console.log(`product id = ${id} and name = ${name}`);
@@ -18,7 +25,7 @@ function runTheLearningSamples() {
   console.log(`${prefix} function declaration`);
   console.log(addNumbersDeclaration(7, 20));
 
-  function addNumbersDeclaration(x: number, y: number) {
+  function addNumbersDeclaration(x: number, y: number): number {
     const sum: number = x + y;
     return sum
   }
@@ -31,4 +38,51 @@ function runTheLearningSamples() {
 
   console.log(`${prefix} function expression`);
   console.log(addNumbersExpression(7, 21));
+
+  const sampleProducts = [
+    {
+      id: 10,
+      name: "Pizza slice",
+      icon: "fas fa-pizza-slice",
+    },
+    {
+      id: 20,
+      name: "Ice cream",
+      icon: "fas fa-ice-cream",
+    },
+    {
+      id: 30,
+      name: "Cheese",
+      icon: "fas fa-cheese",
+    } 
+  ];
+
+  // Return array
+  function getProductNames(): string[] {
+    return sampleProducts.map(p => p.name);
+  }
+
+  console.log(`${prefix} return array`);
+  console.log(getProductNames());
+
+  // Return Product Type
+  function getProductById(id: number): ProductType | undefined {
+    return sampleProducts.find(p => id === p.id);
+  }
+
+  console.log(`${prefix} return ProductType`);
+  console.log(getProductById(10));
+
+  // Return void
+  function displayProducts(products: ProductType[]): void {
+    const productNames = products.map(p => {
+      const name = p.name.toLowerCase();
+      return name;
+    });
+    const msg = `Sample products include: ${productNames.join(", ")}`;
+    console.log(`${prefix} return void`);
+    console.log(msg)
+  }
+
+  displayProducts(sampleProducts);
 }
