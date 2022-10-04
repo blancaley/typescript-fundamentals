cbf8f6e96b27fa14c3684238632643ab2f392508
Blanca Ley <blanca.ley@outlook.com>
2022-10-04 14:49:17 +0200

    Defining functions: declaration and expression

--- src/4-Creating-And-Using-Functions/output.ts
+++ src/4-Creating-And-Using-Functions/output.ts
@@ -13,4 +13,22 @@ function runTheLearningSamples() {
     console.log(`product id = ${id} and name = ${name}`);
   }
   displayProductInfo(10, "pizza");
+
+  // Function declarations are hoisted: If you want to be able to call a function before it's actually declared, then you want to take advantage of the hoisting with function declarations.
+  console.log(`${prefix} function declaration`);
+  console.log(addNumbersDeclaration(7, 20));
+
+  function addNumbersDeclaration(x: number, y: number) {
+    const sum: number = x + y;
+    return sum
+  }
+
+  // Function expression: If you want to make sure that everything is declared before it's being used and enforce that, a function expression can really help you out.
+  const addNumbersExpression = function (x: number, y: number) {
+    const sum: number = x + y;
+    return sum;
+  }
+
+  console.log(`${prefix} function expression`);
+  console.log(addNumbersExpression(7, 21));
 }
