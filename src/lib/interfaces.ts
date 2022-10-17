25697b177a6176f4d44037122fcf33cb7506d530
Blanca Ley <blanca.ley@outlook.com>
2022-10-17 10:23:19 +0200

    Interface vs. Type

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -1,3 +1,5 @@
+// Interface definitions can be used to represent 
+// the shape of an OBJECT-like data structure
 export interface Product {
   id: number;
   name: string;
@@ -7,6 +9,8 @@ export interface Product {
 }
 
 // Examples of using a type alias
+// Type alias declarations can be used to represent
+// primitive types AND object-like data structures.
 type ProductAlias =
   | string
   | number
@@ -25,5 +29,6 @@ enum ProductType {
   Home,
 }
 
+// String literal type, to avoid magic strings
 type ProductTypeList = 'SPORTING' | 'HOME';
 let p: ProductTypeList = 'SPORTING';
