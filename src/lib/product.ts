8cf907d1acd9572abba3c74350006c88b01b759f
Blanca Ley <blanca.ley@outlook.com>
2022-10-17 10:59:01 +0200

    Adding a constructor and properties

--- src/lib/product.ts
+++ src/lib/product.ts
@@ -1,11 +1,21 @@
 // Creating and using a class
-class Product {
-  // properties/fields
+class FoodProduct {
+  // properties/fields.
+  // TS config strict - use default values, empty strings, so they're not undefined at run time
+  id = 0;
+  name = "";
+  icon = "";
   // constructor
+  // Allows to pass in initialization data
+  constructor(id: number, name: string, icon: string) {
+    // Map incoming values to the properties above
+    // "This" represents the instance at run time of this object
+    this.id = id;
+    this.name = name;
+    this.icon = icon;
+  }
   // functions
 }
-
-let product = new Product();
 
 // import { Product } from './interfaces';
 
